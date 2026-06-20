31dd916289e61b1f419c0867125b377bab9a8d1f
econintalacaoemanutencao <ecoinstalacaoemanutencao@gmail.com>
2026-06-19 21:44:08 -0300

    schema jsonld dinamico criado

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,57 +1,229 @@
-export function productSchema(product:any){
-
-  return {
-
-    "@context":"https://schema.org",
-
-    "@type":"Product",
-
-    "name": product.name,
-
-    "description": product.description,
+export function createProductSchema(page:any){
 
 
-    "brand":{
-
-      "@type":"Brand",
-
-      "name":"Crea Gummy"
-
-    },
+return {
 
 
-    "category": product.category,
+"@context":"https://schema.org",
 
 
-    "offers":{
+"@type":"Product",
 
-      "@type":"Offer",
 
-      "priceCurrency":"BRL",
+"name":"Creatina Gummy",
 
-      "availability":
-      "https://schema.org/InStock"
 
-    }
+"description":page.metaDescription,
 
-  }
+
+"brand":{
+
+
+"@type":"Brand",
+
+
+"name":"Creatina Gummy"
+
+},
+
+
+"category":"Suplemento alimentar",
+
+
+"keywords":[
+
+page.keyword,
+
+"creatina gummy",
+
+"creatina em goma",
+
+"suplemento de creatina",
+
+"performance"
+
+],
+
+
+"offers":{
+
+
+"@type":"Offer",
+
+
+"priceCurrency":"BRL",
+
+
+"availability":"https://schema.org/InStock",
+
+
+"url":
+
+`https://creatinagummy.com.br/${page.slug}`
+
 
 }
 
 
 
-export function organizationSchema(){
+};
 
-  return {
 
-    "@context":"https://schema.org",
 
-    "@type":"Organization",
+}
 
-    "name":"Crea Gummy",
 
-    "url":"https://seudominio.com"
 
-  }
+
+
+
+export function createFAQSchema(page:any){
+
+
+
+return {
+
+
+"@context":"https://schema.org",
+
+
+"@type":"FAQPage",
+
+
+"mainEntity":
+
+page.faq.map((item:any)=>(
+
+
+{
+
+
+"@type":"Question",
+
+
+"name":item.question,
+
+
+"acceptedAnswer":{
+
+
+"@type":"Answer",
+
+
+"text":item.answer
+
+
+}
+
+
+
+}
+
+
+
+))
+
+
+};
+
+
+
+}
+
+
+
+
+
+
+export function createBreadcrumbSchema(page:any){
+
+
+
+return {
+
+
+"@context":"https://schema.org",
+
+
+"@type":"BreadcrumbList",
+
+
+"itemListElement":[
+
+
+{
+
+
+"@type":"ListItem",
+
+
+"position":1,
+
+
+"name":"Home",
+
+
+"item":"https://creatinagummy.com.br"
+
+},
+
+
+
+{
+
+
+"@type":"ListItem",
+
+
+"position":2,
+
+
+"name":page.h1,
+
+
+"item":
+
+`https://creatinagummy.com.br/${page.slug}`
+
+
+}
+
+
+
+]
+
+};
+
+
+
+}
+
+
+
+
+
+
+export function createOrganizationSchema(){
+
+
+
+return {
+
+
+"@context":"https://schema.org",
+
+
+"@type":"Organization",
+
+
+"name":"Creatina Gummy",
+
+
+"url":"https://creatinagummy.com.br"
+
+
+};
+
+
 
 }
